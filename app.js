8c2e92f3cd302782ea0478ffe8aee9eeaf11aaf1
Dedi <shuffle.ddie@gmail.com>
2021-03-24 14:42:45 +0700

    JS Fundamental

---
+++ app.js
@@ -0,0 +1,26 @@
+document.getElementById("testing").innerHTML = "Ini Dari Javascript";
+
+var name = "Dedi Setiawan";
+var semester = 8;
+var isFreelance = true;
+var haven = ["mobil", "motor"];//array
+var family = {
+    nama: "Anisa Nurul Hakim",
+    gender: "Perempuan",
+    jabatan: "Istri"
+}//object
+var bigFamily = [{
+    nama: "Anisa Nurul Hakim",
+    gender: "Perempuan",
+    jabatan: "Istri"
+}, {
+    nama: "Ahmed Huffadzulhaqq",
+    gender: "Laki-laki",
+    jabatan: "Anak"
+}
+];//object of array
+console.log(typeof name, typeof semester, typeof isFreelance);
+console.log(haven);
+console.log(family);
+console.log(bigFamily);
+console.log(bigFamily[1].nama);
